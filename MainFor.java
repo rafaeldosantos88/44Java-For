91a29ba589f5c879cae2c8eebc3d985f3b051d27
Rafael dos santos <raphael_979@hotmail.com>
2020-01-27 19:26:39 -0300

    Estrutura de repetição para(for)

---
+++ MainFor.java
@@ -0,0 +1,28 @@
+package estruturaFor;
+
+import java.util.Scanner;
+
+public class MainFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner =new Scanner(System.in);
+		
+			
+		for(int i=60;i>=0;i--) {
+			
+			System.out.println(i);
+			
+			
+		}
+	
+		
+		
+		
+		
+	
+		scanner.close();
+
+	}
+
+}
